b832dc3549e8a366a750d3641adaa07ea01b78c0
Eugen2510 <Lugovoj20@gmail.com>
2023-11-28 13:49:01 +0200

    create note method was changed

--- src/main/java/com/goit/homework15/controller/NoteController.java
+++ src/main/java/com/goit/homework15/controller/NoteController.java
@@ -16,32 +16,31 @@ public class NoteController {
     private final NoteService noteService;
 
     @GetMapping
-    public String getWelcomePage(){
+    public String getWelcomePage() {
         return "welcome_page";
     }
 
     @GetMapping("/list")
-    public ModelAndView getNotes(){
+    public ModelAndView getNotes() {
         ModelAndView modelAndView = new ModelAndView("note_list");
         modelAndView.addObject("notes", noteService.listAll());
         return modelAndView;
     }
 
-    @PostMapping("/create")
+    @RequestMapping("/create")
     public ModelAndView createNote(@RequestParam(name = "title", required = false) String title,
-                             @RequestParam (name = "content", required = false) String content) {
+                                   @RequestParam(name = "content", required = false) String content,
+                                   HttpServletRequest request) {
+        if (request.getMethod().equals("GET")) {
+            return new ModelAndView("create_note");
+        }
         Note note = noteService.createNote(title, content);
         noteService.add(note);
         return getNotes();
     }
 
-    @GetMapping("/create")
-    public String showPageForCreate(){
-        return "create_note";
-    }
-
     @PostMapping("/delete")
-    public ModelAndView deleteNote(@RequestParam(name = "id") long id){
+    public ModelAndView deleteNote(@RequestParam(name = "id") long id) {
         noteService.deleteById(id);
         return getNotes();
     }
@@ -51,9 +50,9 @@ public class NoteController {
     public ModelAndView editNote(@PathVariable(name = "id") long id,
                                  @RequestParam(name = "title", required = false) String title,
                                  @RequestParam(name = "content", required = false) String content,
-                                 HttpServletRequest request){
+                                 HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView();
-        if (request.getMethod().equals("GET")){
+        if (request.getMethod().equals("GET")) {
             modelAndView.setViewName("edit_note");
             modelAndView.addObject("note", noteService.getById(id));
             return modelAndView;
